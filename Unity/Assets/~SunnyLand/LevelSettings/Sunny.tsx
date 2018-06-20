f21afbded6e6eb5df282fc4cc115e8f1cd8084de
AbsolSilver <AbsolSilver@DESKTOP-0L2U9GV>
2018-06-20 19:34:43 +1000

    Made SunnyLand

---
+++ Unity/Assets/~SunnyLand/LevelSettings/Sunny.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Sunny" tilewidth="16" tileheight="16" tilecount="575" columns="25">
+ <image source="../Environment/tileset.png" trans="a2a2a2" width="400" height="368"/>
+</tileset>
